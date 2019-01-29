fe1a9e9038d039c7059ab81545ce8734ecdbf46f
Vyacheclav Chernyshov <vyacheslav.chernyshov@stoloto.ru>
2019-01-29 17:29:09 +0300

    + modelmapper

--- src/main/java/ru/xpendence/modelmapperdemo/ModelmapperDemoApplication.java
+++ src/main/java/ru/xpendence/modelmapperdemo/ModelmapperDemoApplication.java
@@ -1,13 +1,29 @@
 package ru.xpendence.modelmapperdemo;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @SpringBootApplication
 public class ModelmapperDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModelmapperDemoApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
     }
 
 }
